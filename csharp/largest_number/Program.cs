83a060cb2169e17512954d78e7d8521a3ee0fcc1
Thota <rajeswari.thota@dxc.com>
2023-04-20 12:21:23 +0530

    20/4/23 morning excercise added

---
+++ csharp/largest_number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace largest_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int n1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int n2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            int n3= Convert.ToInt32(Console.ReadLine());
+            if(n1>n2 && n1 > n3) 
+            {
+                Console.WriteLine("first number is greater");
+            }
+            else if(n2>n3) 
+            {
+                Console.WriteLine("second number is greater");
+            }
+            else
+            {
+                Console.WriteLine("third number is greater");
+            }
+        }
+    }
+}
---
+++ csharp/vowel/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace vowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("enter an alphabet");
+            ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine("entered character "+ch+" is vowel");
+
+            }
+            else
+            {
+                Console.WriteLine("entered character " + ch + " is not vowel");
+            }
+        }
+    }
+}
