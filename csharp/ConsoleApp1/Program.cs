feac32923dd3bad1cc9d01e4ba2ce66b873f8222
Thota <rajeswari.thota@dxc.com>
2023-05-04 12:17:43 +0530

    updated

---
+++ csharp/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+    class Sortbyprice : IComparer<Product>
+    {
+        public int Compare(Product? x, Product? y)
+        {
+            if(x.Price>y.Price)
+                return -1;
+            else if(x.Price<y.Price)
+                return 1;
+            else return 0;
+
+        }
+    }
+    class SortbyQuantity : IComparer<Product>
+    {
+        public int Compare(Product? x, Product? y)
+        {
+            if (x.Quantity > y.Quantity)
+                return -1;
+            else if (x.Quantity < y.Quantity)
+                return 1;
+            else return 0;
+           
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                 new Product { Id = 1, Name="Laptop", Brand="Dell", Quantity=5, Price=46500},
+                new Product { Id = 2, Name="Camera", Brand="Canon", Quantity=7, Price=32300},
+                new Product { Id = 3, Name="Tablet", Brand="Lenovo", Quantity=2, Price=18500},
+                new Product { Id = 4, Name="Desktop", Brand="HP", Quantity=9, Price=78200},
+                new Product { Id = 5, Name="Earphones", Brand="Boat", Quantity=3, Price=3000},
+                new Product { Id = 6, Name="Mobile", Brand="Apple", Quantity=4, Price=84000},
+            };
+            products.Sort(new Sortbyprice());
+            foreach (Product p in products)
+            {
+                Console.WriteLine($"{p.Id} {p.Name} {p.Brand} {p.Quantity} {p.Price}");
+            }
+            Console.WriteLine();
+            products.Sort(new SortbyQuantity());
+            foreach(Product p in products)
+            {
+                Console.WriteLine($"{p.Id} {p.Name} {p.Brand} {p.Quantity} {p.Price}");
+            }
+        }
+    }
+}
---
+++ csharp/linq_prac/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace linq_prac
+{
+    public class Linq
+    {
+        public int id { get; set; }
+        public string product { get; set; }
+        public string brand { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Linq> list = new List<Linq>()
+            {
+                new Linq {id=1,product="laptop",brand="dell", price=45000, quantity=5},
+                new Linq {id=2,product="mobile",brand="realme", price=15200, quantity=3},
+                new Linq {id=3,product="tab",brand="lenovo", price=20000, quantity=2},
+                new Linq {id=4,product="headset",brand="boat", price=500, quantity=19},
+                new Linq {id=5,product="tv",brand="realme", price=45700, quantity=1}
+            };
+
+     
+
+
+
+
+
+            //foreach (Linq item in query)
+            //{
+            //    Console.WriteLine($"{item.id} {item.product} {item.price} {item.brand} {item.quantity}");
+            //}
+        }
+    }
+}
